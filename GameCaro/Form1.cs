76084ddbb8a0436d8530ec8dfc97d8f2617fc8f4
huynguyen1677 <145359191+huynguyen1677@users.noreply.github.com>
2024-08-21 16:14:56 +0700

    done form

--- GameCaro/Form1.cs
+++ GameCaro/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,35 @@ namespace GameCaro
         public Form1()
         {
             InitializeComponent();
+            Vebanco();
+        }
+        void Vebanco()
+        {
+
+            Button oldButton = new Button() { Width = 0, Location = new Point(0, 0) };
+            for (int i = 0; i < Cons.CHESS_BOARD_HEIGHT; i++)
+            {
+
+
+                for (int j = 0; j < Cons.CHESS_BOARD_WIDTH; j++)
+                {
+                    Button btn = new Button()
+                    {
+                        Width = Cons.CHESS_WIDTH,
+                        Height = Cons.CHESS_HEIGHT,
+                        Location = new Point(oldButton.Location.X + oldButton.Width, oldButton.Location.Y),
+                        BackgroundImageLayout = ImageLayout.Stretch,
+                        Tag = i.ToString()
+                    };
+
+                    chessboard.Controls.Add(btn);
+
+                    oldButton = btn;
+                }
+                oldButton.Location = new Point(0, oldButton.Location.Y + Cons.CHESS_HEIGHT);
+                oldButton.Width = 0;
+                oldButton.Height = 0;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,6 +53,21 @@ namespace GameCaro
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picturelogouth_Click(object sender, EventArgs e)
         {
 
         }
